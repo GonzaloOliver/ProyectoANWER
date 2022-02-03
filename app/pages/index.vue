ad3896b720caa9fc47ff80bd979420905bf7a2c4
GonzaloOliver <olivergonzalo12@gmail.com>
2022-02-03 15:54:19 -0300

    dsd

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,9 +2,7 @@
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden" style="width: 55%;text-align: justify;margin-right: 25px;">
-       
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
-        </div>
 
         </div> 
   		</div>
