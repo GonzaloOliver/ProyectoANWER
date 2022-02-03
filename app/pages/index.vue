a9e10ec35661ffd42c871c59a932b56992ebb94c
GonzaloOliver <olivergonzalo12@gmail.com>
2022-02-03 13:14:38 -0300

    jpg

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -37,7 +37,7 @@
         <img
           alt="Hero"
           class="rounded shadow-xl"
-          src="/images/uploads/B.pgn"
+          src="/images/uploads/B.jpg"
         />
       </div>
     </div>
